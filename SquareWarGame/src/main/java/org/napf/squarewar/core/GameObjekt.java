987bbac3e5242259e6de114468039adab22f69d0
niklaswinter <niklaswinter@localhost>
2023-06-21 09:15:48 +0200

    nikloses package core

--- SquareWarGame/src/main/java/org/napf/squarewar/core/GameObjekt.java
+++ SquareWarGame/src/main/java/org/napf/squarewar/core/GameObjekt.java
@@ -4,10 +4,13 @@ public class GameObject {
    private double xpos;
    private double ypos;
    private String name;
+   private Appearance appearance;
    
    public GameObject(double xpos, double ypos){
 	
+	   
 	   this.xpos = xpos;
+	   this.ypos = ypos;
    }
 
 }
