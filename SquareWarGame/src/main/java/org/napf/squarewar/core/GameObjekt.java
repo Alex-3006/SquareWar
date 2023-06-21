2d62a1222651b15275b5c64ac73064fbb65c6fff
niklaswinter <niklaswinter@localhost>
2023-06-21 08:42:18 +0200

    added package core

---
+++ SquareWarGame/src/main/java/org/napf/squarewar/core/GameObjekt.java
@@ -0,0 +1,13 @@
+package org.napf.squarewar.core;
+
+public class GameObject {
+   private double xpos;
+   private double ypos;
+   private String name;
+   
+   public GameObject(double xpos, double ypos){
+	
+	   this.xpos = xpos;
+   }
+
+}
