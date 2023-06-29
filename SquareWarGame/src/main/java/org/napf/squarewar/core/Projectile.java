6e8c5bb4dd679b9881ff1bebc03039d8a6cce832
nikla <nikla@DESKTOP-992J13P.fritz.box>
2023-06-29 19:56:33 +0200

    better projektile movemant with quickmaths

--- SquareWarGame/src/main/java/org/napf/squarewar/core/Projectile.java
+++ SquareWarGame/src/main/java/org/napf/squarewar/core/Projectile.java
@@ -8,31 +8,33 @@ public class Projectile extends PhysicsObject{
   private double ypos;
   private double adjacent;
   private double opposite;  
+  private boolean quickmaths;
   
   public Projectile(double xpos, double ypos) {
 	  super(xpos, ypos);
 	  this.xpos = xpos;
 	  this.ypos = ypos;
+	  adjacent = 0;
+	  opposite = 0;
+	  quickmaths = true;
   }
  
   public void update(int mouseXCord, int mouseYCord, int tankXCord, int tankYCord){
 	  double h;
       double deg;
-      double adjacent;
-      double opposite;
+      while( quickmaths == true) {
 	  h = Math.sqrt(Math.pow((tankXCord - mouseXCord),2) + Math.pow((tankYCord - mouseYCord),2));
 	  deg = Math.acos((tankXCord - mouseXCord) / h);
 	  adjacent = Math.cos(deg) * speed;
 	  opposite = Math.cos(deg) * speed;
-	  while(true) {
-		  x = xpos + adjacent;
-		  y = ypos + opposite;
-		  moveTo(x, y);
-		  x = xpos;
-		  y = ypos;
-	  }
-	  
+	  quickmaths = false;
+      }
+	  x = xpos + adjacent;
+	  y = ypos + opposite;
+	  moveTo(x, y);
+	  x = xpos;
+	  y = ypos;
+	    
   }
-  
-  
+   
 }
