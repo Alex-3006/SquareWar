6db068b03e355af78a9b37669eab08a31fa2b1d4
gkont <gkont@LAPTOP-4QKG9QCF>
2023-07-01 15:24:40 +0200

    updated getter/setter methods class Appearance

--- SquareWarGame/src/main/java/org/napf/squarewar/core/Appearance.java
+++ SquareWarGame/src/main/java/org/napf/squarewar/core/Appearance.java
@@ -6,11 +6,17 @@ public class Appearance {
 	private double opacity;
 	private double rotation;
 	
-	public Appearance
+	public Appearance() {
+		appearanceComponents = new AppearanceComponent[1000];
+	}
 	
 	//Implementierung Getter-Methoden
 	
-	public AppearanceComponent[] getAppearanceComponents(int positionInArray) {
+	public AppearanceComponent getAppearanceComponent(int positionInArray) {
+		return appearanceComponents[positionInArray];
+	}
+	
+	public AppearanceComponent[] getAppearanceComponents() {
 		return appearanceComponents;
 	}
 	
@@ -24,8 +30,21 @@ public class Appearance {
 	
 	//Implementierung Setter-Methoden
 	
-	public void setAppearanceComponents() {
-		
+	public void setAppearanceComponent(int positionInArray, AppearanceComponent appearanceComponent) {
+		appearanceComponents[positionInArray] = appearanceComponent;
 	}
-
+	
+	public void setOpacity(double opacity) {
+		if(opacity >= 0 && opacity <= 100) {
+			this.opacity = opacity;
+		} else {
+			System.out.println("Value Opacity = wrong, Value range: 0-100 %");
+		}
+	}
+	
+	public void setRotation(double rotation) {
+		if(rotation >= 0 && rotation <= 360) {
+			this.rotation = rotation;
+		}
+	}
 }
