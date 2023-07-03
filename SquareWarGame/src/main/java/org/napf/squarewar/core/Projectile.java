2a671e36a589dc1582aa75ea0a2442c77852c7ee
nikla <nikla@DESKTOP-992J13P.fritz.box>
2023-07-03 17:44:18 +0200

    arrrrrrrrrrrrrrrghhhhhhhhhh + projectile

--- SquareWarGame/src/main/java/org/napf/squarewar/core/Projectile.java
+++ SquareWarGame/src/main/java/org/napf/squarewar/core/Projectile.java
@@ -1,9 +1,54 @@
-package org.napf.squarewar.core;
+ package org.napf.squarewar.core;
+ import org.napf.util.NapfMath;
+
 
 public class Projectile extends PhysicsObject{
-  private float speed;
-  
+  protected float speed;
+  private double x;
+  private double y;
+  private double xpos;
+  private double ypos;
+  private double adjacent;
+  private double opposite;
+  private boolean quickmaths;
+
   public Projectile(double xpos, double ypos) {
-	  super(xpos, ypos);
+      super(xpos, ypos);
+      this.xpos = xpos;
+      this.ypos = ypos;
+      adjacent = 0;
+      opposite = 0;
+      quickmaths = true;
+  }
+ 
+  public void update(int mouseXCord, int mouseYCord, int tankXCord, int tankYCord){
+      /*double h;
+      double deg; 
+      while( quickmaths == true) {
+      h = Math.sqrt(Math.pow((tankXCord - mouseXCord),2) + Math.pow((tankYCord - mouseYCord),2));
+      deg = Math.acos((tankXCord - mouseXCord) / h);
+      adjacent = Math.cos(deg) * speed;
+      opposite = Math.cos(deg) * speed;
+      quickmaths = false;
+      }
+      x = xpos + adjacent;
+      y = ypos + opposite;
+      moveTo(x, y);
+      xpos = x;
+      ypos = y;*/
+
+      double dirX, dirY;
+
+      dirX = mouseXCord - tankXCord;
+      dirY = mouseYCord - tankYCord;
+    // Convert into unit vector
+      if (!(dirX == 0 && dirY == 0)) {
+			double magnitude = NapfMath.magnitude2D(dirX, dirY);
+			dirX /= magnitude;
+			dirY /= magnitude;
+			moveBy(dirX * speed * GameManager.getInstance().getDeltaTime(), dirY * speed * GameManager.getInstance().getDeltaTime());
+
+      }
+
   }
 }
